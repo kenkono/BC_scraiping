482958a3a5c83fa3caccf373ec7680ac2644f9e7
EAD\kkono <re1college.box@gmail.com>
2024-08-31 11:42:50 +0900

    add price and point

--- ProductExtractor.py
+++ ProductExtractor.py
@@ -8,15 +8,27 @@ class ProductExtractor:
 
     def extract_products(self, html_content):
         products = []
-        # Parse the HTML content with BeautifulSoup
+
+        # BeautifulSoupを使用してHTMLコンテンツを解析する
         soup = BeautifulSoup(html_content, 'html.parser')
 
-        # Extract all product names within the class 'bcs_item'
+        # クラス 'bcs_itemBox' 内の 'bcs_title' を持つすべての製品名を抽出する
         product_names = soup.select('div.bcs_itemBox p.bcs_title a.bcs_item')
+        # クラス 'bcs_price' を持つすべての価格を抽出する
+        prices = soup.select('p.bcs_price')
+        # クラス 'bcs_point' を持つすべてのポイントを抽出する
+        points = soup.select('p.bcs_point')
 
-        for product in product_names:
-            products.append(product.get_text(strip=True))
-        # 製品名を返す
+        # 製品名、価格、ポイントをリストに格納する
+        for name, price, point in zip(product_names, prices, points):
+            product_info = {
+                'name': name.get_text(strip=True),
+                'price': price.get_text(strip=True),
+                'point': point.get_text(strip=True)
+            }
+            products.append(product_info)
+
+        # 製品情報を返す
         print(products)
         return products
 
@@ -29,7 +41,7 @@ if __name__ == "__main__":
     extractor = ProductExtractor()
 
     # 1ページ目と2ページ目のHTMLコンテンツを取得
-    pages_content = fetcher.fetch_pages(1, 2)
+    pages_content = fetcher.fetch_pages(1, 3)
 
     # 各ページの製品名を抽出して表示
     for content in pages_content:
