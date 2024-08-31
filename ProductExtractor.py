20bacdc9a013381de6127db01d7cde4ad2899586
EAD\kkono <re1college.box@gmail.com>
2024-08-31 15:30:49 +0900

    feat: Refactor code to fetch and extract products from multiple pages

--- ProductExtractor.py
+++ ProductExtractor.py
@@ -32,31 +32,6 @@ class ProductExtractor:
                 'point': point
             }
             products.append(product_info)
-
-        # デバッグ用に抽出した製品情報を出力
-        print("Extracted products:", products)
-
         # 製品情報を返す
         return products
 
-# 使用例
-if __name__ == "__main__":
-    fetcher = PageFetcher()
-    extractor = ProductExtractor()
-
-    # 1ページ目と2ページ目のHTMLコンテンツを取得
-    pages_content = fetcher.fetch_pages(1, 2)
-
-    # 各ページの製品名を抽出して表示
-    all_products = []
-    for content in pages_content:
-        products = extractor.extract_products(content)
-        all_products.extend(products)
-
-    # 抽出した全製品情報を出力
-    print("All extracted products:", all_products)
-
-    # CSVWriterを使用して製品情報をCSVに書き込む
-    from CSVWriter import CSVWriter
-    csv_writer = CSVWriter()
-    csv_writer.write_to_csv(all_products)
